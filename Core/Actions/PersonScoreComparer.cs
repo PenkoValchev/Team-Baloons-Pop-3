274e2eaf26808419eeb6170ff861a3f450225942
luboganchev <luboganchev91@gmail.com>
2014-07-12 19:47:16 +0300

    PersonScoreComparer Revert

---
+++ Core/Actions/PersonScoreComparer.cs
@@ -0,0 +1,16 @@
+﻿namespace BalloonsPops.Core.Actions
+{
+    ﻿using BalloonsPops.Core.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+
+    public class PersonScoreComparer : IComparer<IPlayer>
+    {
+        public int Compare(IPlayer x, IPlayer y)
+        {
+            return x.Score.CompareTo(y.Score);
+        }
+    }
+}
