982575b7a8e16c39b488110e22fc12658754a414
PenkoValchev <nekavuser@abv.bg>
2014-07-16 16:47:11 +0300

    Player Unit Test Final 100% coverage

--- BaloonsUnitTest/Playertest.cs
+++ BaloonsUnitTest/Playertest.cs
@@ -37,6 +37,49 @@ namespace BaloonsUnitTest
             Player player = new Player("1");
         }
 
+         [TestMethod]
+         public void PlayerIfNameIsSaveCorrect()
+         {
+             string expected = "Pesho";
+             Player player = new Player(expected);
+             Assert.AreEqual(expected, player.Name);
+         }
+
+         [TestMethod]
+         [ExpectedException(typeof(ArgumentException), "Score can't be less than zero.")]
+         public void PlayerIfScoreLessThanZero() 
+         {
+             Player player = new Player("Pesho",-1);
+         }
+
+         [TestMethod]
+         public void PlayerIfScoreIsSaveCorrect()
+         {
+             int expected = 3;
+             Player player = new Player("Pesho", expected);
+             Assert.AreEqual(expected, player.Score);
+         }
+
+
+         [TestMethod]
+         public void PlayerIsGreaterThanAnother()
+         {
+             bool expected = true;
+             Player firstPlayer = new Player("Pesho",10);
+             Player secondPlayer = new Player("Gosho",6);
+             Assert.AreEqual(expected, firstPlayer > secondPlayer);
+         }
+
+
+         [TestMethod]
+         public void PlayerIsLessThanAnother()
+         {
+             bool expected = true;
+             Player firstPlayer = new Player("Pesho", 1);
+             Player secondPlayer = new Player("Gosho", 6);
+             Assert.AreEqual(expected, firstPlayer < secondPlayer);
+         }
+        
         
     }
 }
